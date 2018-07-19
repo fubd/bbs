bfbafb2c6bdd8d8f74787a4363efcfa8483bf697
fubd <fubd@promote.cache-dns.local>
2018-07-19 20:48:16 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        南朝四百八十寺，多少楼台烟雨中。
+      </div>
+    );
+  }
+}
+
+export default App;
